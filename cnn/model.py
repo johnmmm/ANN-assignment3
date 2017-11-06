586e616632d4ba1b934459e2083b429882a785fc
周京汉 <zhoujh15@mails.tsinghua.edu.cn>
2017-11-06 17:18:21 +0800

    begin to make the parameters better in cnn.

--- cnn/model.py
+++ cnn/model.py
@@ -8,8 +8,8 @@ from tensorflow.python.training import moving_averages
 class Model:
     def __init__(self,
                  is_train,
-                 learning_rate=0.002,
-                 learning_rate_decay_factor=0.975):
+                 learning_rate=0.0015,
+                 learning_rate_decay_factor=0.95):
         self.x_ = tf.placeholder(tf.float32, [None, 1, 28, 28])
         self.y_ = tf.placeholder(tf.int32, [None])
         self.keep_prob = tf.placeholder(tf.float32)
@@ -20,30 +20,30 @@ class Model:
         #        the 10-class prediction output is named as "logits"
 
         #第一个卷积
-        W_conv1 = weight_variable([5, 5, 1, 4])
-        b_conv1 = bias_variable([4])
+        W_conv1 = weight_variable([5, 5, 1, 32])
+        b_conv1 = bias_variable([32])
 
         h_conv1 = conv2d(x, W_conv1) + b_conv1
-        h_bn1 = batch_normalization_layer(h_conv1)
+        #h_bn1 = batch_normalization_layer(h_conv1)
 
-        h_relu1 = tf.nn.relu(h_bn1)
+        h_relu1 = tf.nn.relu(h_conv1)
         h_pool1 = max_pool_2x2(h_relu1)
 
         #第二个卷积
-        W_conv2 = weight_variable([5, 5, 4, 8])
-        b_conv2 = bias_variable([8])
+        W_conv2 = weight_variable([5, 5, 32, 64])
+        b_conv2 = bias_variable([64])
 
         h_conv2 = conv2d(h_pool1, W_conv2) + b_conv2
-        h_bn2 = batch_normalization_layer(h_conv2)
+        #h_bn2 = batch_normalization_layer(h_conv2)
 
-        h_relu2 = tf.nn.relu(h_bn2)
+        h_relu2 = tf.nn.relu(h_conv2)
         h_pool2 = max_pool_2x2(h_relu2)
 
         #Linear
-        W_fc1 = weight_variable([7 * 7 * 8, 10])
+        W_fc1 = weight_variable([7 * 7 * 64, 10])
         b_fc1 = bias_variable([10])
 
-        h_pool2_flat = tf.reshape(h_pool2, [-1, 7*7*8])
+        h_pool2_flat = tf.reshape(h_pool2, [-1, 7*7*64])
         logits = tf.matmul(h_pool2_flat, W_fc1) + b_fc1
 
         self.loss = tf.reduce_mean(tf.nn.sparse_softmax_cross_entropy_with_logits(labels=self.y_, logits=logits))
